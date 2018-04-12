73f9eae3e81d8cf67d6cd8c95e5ca97b8ea2f708
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-04-11 22:39:33 -0500

    Add function to sum numbers in array

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -5,3 +5,14 @@ end
 def subtract a, b
   return a - b
 end
+
+def sum numbers
+  if numbers == []
+    return 0
+  end
+  answer = 0
+  numbers.each_with_index do |n, i|
+    answer += n
+  end
+  return answer.to_i
+end
