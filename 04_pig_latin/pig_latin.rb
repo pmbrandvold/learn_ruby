87eecb2a82430668e38a1494afefb91f234f02b2
Phil Brandvold <phillip.brandvold@gmail.com>
2018-04-12 16:38:21 -0500

    Translate single word

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
-#write your code here
+def translate phrase
+  old_text = phrase.split("")
+  vowels = ['a', 'e', 'i', 'o']
+  before_vowel = []
+  after_vowel = []
+  hit_vowel = false
+  old_text.each do |l|
+    if vowels.include?(l)
+      hit_vowel = true
+    end
+    if hit_vowel == false
+      before_vowel.push(l)
+    else
+      after_vowel.push(l)
+    end
+  end
+  string = after_vowel.join("") + before_vowel.join("") + "ay"
+  return string
+end 
+
+
