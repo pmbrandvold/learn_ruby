f38ca3f89acbcf4bd89071a57512fc2bcb7cbe66
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-04-13 13:02:18 -0500

    Display seconds

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,38 @@
 class Timer
-  #write your code here
+  attr_accessor :timer, :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = 0
+    minutes = 0
+    display_hours = ''
+    display_minutes = ''
+    if @seconds > 60
+      minutes += 1
+      @seconds = 0
+    end
+    if minutes > 60
+      hours += 1
+      minutes = 0
+    end
+    if hours < 10
+      display_hours = "0" + hours.to_s
+    else
+      display_hours = hours.to_s
+    end
+    if minutes < 10
+      display_minutes = "0" + minutes.to_s
+    else
+      display_minutes = minutes.to_s
+    end
+    if seconds < 10
+      display_seconds = "0" + seconds.to_s
+    else
+      display_seconds = seconds.to_s
+    end
+    display = display_hours + ":" + display_minutes + ":" + display_seconds
+  end
 end
