b85d9bf8bd09155fd97ba1c5bb913787e7a07170
Phillip Brandvold <phillip.brandvold@gmail.com>
2018-04-12 21:54:04 -0500

    Capitalize multiple word titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,9 +1,13 @@
 class Book
   def title=(title)
-    @title = title
+    @title = title.split(" ")
   end
 
   def title
-    @title.capitalize
+    new_title = []
+    @title.each do |w|
+      new_title.push(w.capitalize)
+    end
+    new_title.join(" ")
   end
 end
